8b15040554ef810855e1a981f870218d6bb752ac
sysadm <sysadm>
2016-12-03 03:47:54 +0100

    Manage https

--- server.js
+++ server.js
@@ -1,27 +1,19 @@
 var express = require('express');
+
+// Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
 
-const port = process.env.PORT || 3000;
-
-// Manage https request
-/*app.use(function (req, res, next) {
-    if (req.headers['x-forward-proto'] == 'https') {
-        res.redirect(`http://${req.hostname}${req.url}`);
+app.use(function (req, res, next){
+    if (req.headers['x-forwarded-proto'] === 'https') {
+        res.redirect('http://' + req.hostname + req.url);
     } else {
         next();
     }
-});*/
+});
 
 app.use(express.static('public'));
 
-/*
-app.use('/scripts', express.static(__dirname + '/node_modules/babel-core/lib/api'));
-app.use('/scripts', express.static(__dirname + '/node_modules/react/dist'));
-app.use('/scripts', express.static(__dirname + '/node_modules/react-dom/dist'));
-*/
-
-app.listen(port, function () {
-    console.log(`Express server is up on port ${port}`);
+app.listen(PORT, function () {
+    console.log('Express server is up on port ' + PORT);
 });
-
-
