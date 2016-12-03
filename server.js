741deb9f2a7f7a6dea12bc4aecb7ceb123eadec4
sysadm <sysadm>
2016-12-03 03:35:02 +0100

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 var express = require('express');
 var app = express();
 
+const port = process.env.PORT || 3000;
+
+// Manage https request
+app.use(function (req, res, next) {
+    if (req.headers['x-forward-proto'] == 'https') {
+        res.redirect(`http://${req.hostname}${req.url}`);
+    } else {
+        next();
+    }
+});
+
 app.use(express.static('public'));
 
+/*
 app.use('/scripts', express.static(__dirname + '/node_modules/babel-core/lib/api'));
 app.use('/scripts', express.static(__dirname + '/node_modules/react/dist'));
 app.use('/scripts', express.static(__dirname + '/node_modules/react-dom/dist'));
-app.use('/stylesheets', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+*/
 
-
-app.listen(3000, function () {
-    console.log('Express server is up on port 3000');
+app.listen(port, function () {
+    console.log(`Express server is up on port ${port}`);
 });
 
 
